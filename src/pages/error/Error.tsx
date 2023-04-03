b3a2ee37531204c8db1f7e8bb9f9ea649241a6e1
Oya-Tomo <oyatomo.dev@gmail.com>
2023-04-03 22:30:31 +0900

    updated: added not-fount page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { PageFooter, PageHeader } from "../../components";
+import style from "./Error.module.scss";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className={style["page"]}>
+      <PageHeader />
+      <main className={style["main"]}>
+        <h1>Oops !! Not Found ...</h1>
+        <p>どんまい。</p>
+      </main>
+      <PageFooter />
+    </div>
+  );
 };
 
 export default Error;
